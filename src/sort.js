f44897d0f97546e4558c603af3c6428735f97e2f
David Dorward <david@dorward.me.uk>
2020-07-20 13:07:10 +0100

    Reformat

--- src/sort.js
+++ src/sort.js
@@ -1,14 +1,13 @@
-import read from "./read.js";
+import read from './read.js';
 
 export default async function sortPromise(options) {
-	console.log({options});
+	// console.log({options});
 	const type_path = `${options.project}/data/categories.json`;
-	console.log({type_path});
+	// console.log({type_path});
 	const detailed_types = JSON.parse(await read(type_path));
-	const types = detailed_types.map((type) => type.category);
-	const sort = (data) => data.sort(rules);
-	const by_date = (entry) =>
-		detailed_types.find((c) => c.category === entry.type).sort === "date";
+	const types = detailed_types.map(type => type.category);
+	const sort = data => data.sort(rules);
+	const by_date = entry => detailed_types.find(c => c.category === entry.type).sort === 'date';
 	const rules = (a, b) => {
 		if (by_date(a) && by_date(b)) {
 			// Only some kinds of record should be sorted by date
@@ -22,5 +21,5 @@ export default async function sortPromise(options) {
 		return 0;
 	};
 
-	return {sort};
+	return { sort };
 }
