a52e1ffc94617873e4d2303d4a3020707ef869f7
David Dorward <david@dorward.me.uk>
2020-07-20 13:06:54 +0100

    New formatting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,21 @@
 module.exports = {
-	"env": {
-		"es6": true,
-		"node": true
+	env: {
+		es6: true,
+		node: true,
 	},
-	"extends": ["eslint:recommended", "plugin:import/errors"],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	extends: ['eslint:recommended', 'plugin:import/errors'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
 	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
 	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
