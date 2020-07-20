90c02ea48b83f5307a913b8f7c428cdb1ab027a8
David Dorward <david@dorward.me.uk>
2020-07-20 13:08:08 +0100

    Clean up debugging

--- src/add_title_attributes.js
+++ src/add_title_attributes.js
@@ -1,6 +1,8 @@
 export default ($) => {
+	// console.log("Adding title attributes");
 	$("a").each(
 		(i, a) => {
+			// console.log({a, i});
 			const href = $(a).attr("href");
 			const heading = $(`${href} h2`).text();
 			$(a).attr("title", heading);
