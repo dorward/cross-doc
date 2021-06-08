fabc221d8690f69a15f96f4de715fca960276572
David Dorward <david@dorward.me.uk>
2021-06-08 19:59:28 +0100

    Code formatting

--- src/get_files.js
+++ src/get_files.js
@@ -1,19 +1,19 @@
-import klaw from "klaw";
+import klaw from 'klaw';
 
 const isMd = /\.md$/;
 
-export default (options) => {
+export default options => {
 	const paths = [];
-	return new Promise( (res) => {
+	return new Promise(res => {
 		klaw(`${options.project}/data/`)
-			.on("data", item => {
+			.on('data', item => {
 				paths.push(item.path);
 			})
-			.on("end", () => {
+			.on('end', () => {
 				const mds = paths.filter(file => isMd.test(file));
-				const {include} = options;
+				const { include } = options;
 				if (!include) return res(mds);
-				if (!Array.isArray(include)) throw "Include is not an array";
+				if (!Array.isArray(include)) throw 'Include is not an array';
 				const name_regex = /([^/]+)\.md$/;
 				const included = mds.filter(file => {
 					const name = file.match(name_regex)[1];
@@ -22,5 +22,4 @@ export default (options) => {
 				res(included);
 			});
 	});
-	
 };
