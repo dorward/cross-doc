332d91deb068d1fbd3e1c1a2b9bb4126394f3151
David Dorward <david@dorward.me.uk>
2022-09-08 09:09:09 +0100

    Upgrade marked for security reasons

--- src/parse_data.js
+++ src/parse_data.js
@@ -1,20 +1,18 @@
-import marked from "marked";
-import {loadFront} from "yaml-front-matter";
-import { basename } from "path";
+import { marked } from 'marked';
+import { loadFront } from 'yaml-front-matter';
+import { basename } from 'path';
 
-export default (data) => data.map(parse_datum);
+export default data => data.map(parse_datum);
 
-const parse_datum = (datum) => {
+const parse_datum = datum => {
 	const { path, contents } = datum;
 	const filename = basename(path);
-	const slug = filename.replace(/\.md/, "");
+	const slug = filename.replace(/\.md/, '');
 	try {
 		const parsed = loadFront(contents);
-		if (!parsed.type) 
-			throw "Missing mandatory 'type' attribute in the front matter";
-		if (!parsed.title) 
-			throw "Missing mandatory 'title' attribute in the front matter";
-		const html = marked(parsed.__content, { smartLists: true, smartypants: true });
+		if (!parsed.type) throw "Missing mandatory 'type' attribute in the front matter";
+		if (!parsed.title) throw "Missing mandatory 'title' attribute in the front matter";
+		const html = marked.parse(parsed.__content, { smartLists: true, smartypants: true });
 		return { html, path, slug, ...parsed };
 	} catch (e) {
 		process.stderr.write(`Error handling ${slug}: ${e}`);
